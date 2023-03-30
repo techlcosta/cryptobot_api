d7a8295470f02c390530aa602cd962e703115ab7
Leandro Costa <techlcosta@Leandros-MacBook-Pro.local>
2023-03-30 19:27:43 +0900

    add save settings route

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,5 +1,6 @@
 import { type FastifyInstance } from 'fastify'
 import { getUserProfileController } from '../controllers/getUserProfile-controller'
+import { saveSettingsController } from '../controllers/saveSettings-controller'
 import { userAuthenticateController } from '../controllers/userAuthenticate-controller'
 import { userRefreshTokenController } from '../controllers/userRefreshToken-controller'
 import { userRegisterController } from '../controllers/userRegister-controller'
@@ -13,4 +14,5 @@ export async function appRoutes (app: FastifyInstance): Promise<void> {
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, getUserProfileController)
+  app.post('/settings/save', { onRequest: [verifyJWT] }, saveSettingsController)
 }
